a6d06b5489386d298fa9405f58e0ed032ac9d458
doomed-phobos <the_ekj.2121@outlook.com>
2022-02-20 07:55:29 -0500

    Added App class

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -1,2 +1,31 @@
 #include "app/app.hpp"
 
+#include "app/ui/main_window.hpp"
+
+#include <cassert>
+#include <gtkmm/application.h>
+
+#define MARS_APPLICATION_ID "com.phobos.mars"
+
+namespace app {
+   static Glib::RefPtr<Gtk::Application> s_gtkApp(nullptr);
+   App* App::s_instance = nullptr;
+
+   App::App(int argc, char* argv[]) {
+      assert(!s_instance);s_instance = this;
+
+      s_gtkApp = Gtk::Application::create(argc, argv, MARS_APPLICATION_ID);
+      mMainWindow = new MainWindow();
+   }
+
+   App::~App() {
+      assert(s_instance);s_instance = nullptr;
+      s_gtkApp.reset();
+
+      delete mMainWindow;
+   }
+
+   int App::run() const {
+      return s_gtkApp->run(*mMainWindow);
+   }
+} // namespace app
--- src/app/app.hpp
+++ src/app/app.hpp
@@ -2,8 +2,18 @@
 #define _MARS_APP_HPP
 
 namespace app {
-   class Application {
+   class MainWindow;
 
+   class App {
+   public:
+      App(int argc, char* argv[]);
+      ~App();
+
+      int run() const;
+   private:
+      static App* s_instance;
+
+      MainWindow* mMainWindow;
    };
 } // namespace app
 
